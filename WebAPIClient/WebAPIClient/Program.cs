9a6bdf55163a8fd5f0251eec889bb0e7c9f4deda
arham83 <arhamriaz83@gmail.com>
2023-05-18 18:54:59 +0500

    Web API POC

--- WebAPIClient/WebAPIClient/Program.cs
+++ WebAPIClient/WebAPIClient/Program.cs
@@ -16,8 +16,31 @@ namespace WebAPIClient
     {
         static async Task Main(string[] args)
         {
-            
-            var summary2 = BenchmarkRunner.Run<BenchMarkMessagePack>();
+            APICalls req = new APICalls("http://localhost:32872/values");
+
+           
+           SampleMessage sm = new SampleMessage()
+            {
+                Id = 306,
+                Name = "Arham"
+            };
+            var data = MessagePackSerializer.Serialize(sm);
+
+            var response = await req.Post(data,"/SM");
+            var SFM = JsonHandler.DeserializeJson<FullMessage>(Misc.GetPath(@"SampleMessages\smallFull.json"));
+            var BFM = JsonHandler.DeserializeJson<FullMessage>(Misc.GetPath(@"SampleMessages\bigFull.json"));
+            var SOM = JsonHandler.DeserializeJson<OptimizedMessage>(Misc.GetPath(@"SampleMessages\smallOptimized.json"));
+            var BOM = JsonHandler.DeserializeJson<OptimizedMessage>(Misc.GetPath(@"SampleMessages\bigOptimized.json"));
+            var mpBytes1 = MessagePackSerializer.Serialize(SFM);
+            var mpBytes2 = MessagePackSerializer.Serialize(SOM);
+            var mpBytes3 = MessagePackSerializer.Serialize(BFM);
+            var mpBytes4 = MessagePackSerializer.Serialize(BOM);
+
+            Console.WriteLine("SFM" + mpBytes1.Length);
+            Console.WriteLine("SOM" + mpBytes2.Length);
+            Console.WriteLine("BFM" + mpBytes3.Length);
+            Console.WriteLine("BOM" + mpBytes4.Length);
+            //var summary2 = BenchmarkRunner.Run<BenchMarkMessagePack>();
 
             Console.ReadLine();
         }
